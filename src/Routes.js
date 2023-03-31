1798f7b76a2f63fd2c0035631a65d763a09d0cb5
viraj <viraj@uxdlabtech.com>
2023-03-31 16:59:14 +0530

    ui changes

---
+++ src/Routes.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Sidebar } from "./Components/Sidebar/Sidebar";
+import { Home } from "./Components/home/Home";
+
+export const Routes = () => {
+    return (
+        <>
+            <Home />
+        </>
+    )
+}
