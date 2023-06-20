b36e9ef3c3b96464c24a04b3e080843990c672a8
Yeshraj@12 <yeshraj@uxdlabtech.com>
2023-06-20 17:34:22 +0530

    snackbar in customer delete

--- src/Components/Users/UsersListing.jsx
+++ src/Components/Users/UsersListing.jsx
@@ -3,6 +3,7 @@ import './Style.css'
 import { Box, Switch, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
 import Pagination from 'rc-pagination';
 import { getAllUsers, activeUser, deleteUsers } from "../../services/Users";
+import { SnackBar } from "../Assets/SnackBar";
 import { Delete, Edit } from '@mui/icons-material';
 
 
@@ -14,6 +15,13 @@ export default function UsersListing() {
     const [collection, setCollection] = React.useState(
         (allUsers.slice(0, countPerPage))
     );
+     const [snackbar, ShowSnackbar] = useState({
+       show: false,
+       vertical: "top",
+       horizontal: "right",
+       msg: "data added",
+       type: "error",
+     });
 
     React.useEffect(() => {
         if (!value) {
@@ -34,7 +42,14 @@ export default function UsersListing() {
     const deleteusers = (id)=>{
         deleteUsers(id)
         .then(res =>{console.log(res)
-         getUsers();})
+         getUsers();
+         ShowSnackbar({
+           show: true,
+           vertical: "top",
+           horizontal: "right",
+           msg: "Product Deleted successfully",
+           type: "success",
+         });})
         .catch(err =>{console.log(err)})
     }
 
@@ -45,69 +60,81 @@ export default function UsersListing() {
         setCollection(allUsers.slice(from, to));
     };
     return (
-        <>
-            <div class="container-fluid">
-                <div class="col mt-2 d-flex justify-content-between">
-                    <h1 class="d-inline-block">Customers</h1>
-                </div>
-            </div>
-            <Box sx={{ mx: 2 }} className='border'>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            {/* <TableCell className="w-12"><b>S.No</b></TableCell> */}
-                            <TableCell className="w-25"><b>Name</b></TableCell>
-                            <TableCell><b>Email</b></TableCell>
-                            <TableCell><b>Phone No.</b></TableCell>
-                            <TableCell><b>Active</b></TableCell>
-                            <TableCell><b>Actions</b></TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {collection.map((res, index) => {
-                            return (
-                              <TableRow key={index}>
-                                {/* <TableCell>{index + 1}</TableCell> */}
-                                <TableCell className="text_cap">
-                                  {res.fullName}
-                                </TableCell>
-                                <TableCell>{res.email}</TableCell>
-                                <TableCell>{res.mobile_number}</TableCell>
-                                <TableCell>
-                                  <Switch
-                                    defaultChecked={res.user_login}
-                                    onChange={() => active(res._id)}
-                                  />
-                                </TableCell>
-                                <TableCell>
-                                  {/* <RemoveRedEye  /> */}
-                                  <Delete
-                                    className="pointer"
-                                    onClick={e=>deleteusers(res._id)}
-                                  />
-                                  &nbsp;&nbsp;
-                                  {/* <Edit className="pointer" /> */}
-                                </TableCell>
-                              </TableRow>
-                            );
-                        })}
-                    </TableBody>
-                </Table>
-                <Box sx={{ m: 1 }} className='d-flex justify-content-end'>
-                    <select className="me-2" onChange={(e) => setCountPerPage(e.target.value * 1)}>
-                        <option>5</option>
-                        <option>10</option>
-                        <option>15</option>
-                    </select>
-                    <Pagination
-                        pageSize={countPerPage}
-                        onChange={updatePage}
-                        current={currentPage}
-                        total={allUsers.length}
-                        style={{ color: 'green' }}
-                    />
-                </Box>
-            </Box>
-        </>
-    )
+      <>
+        <SnackBar snackBarData={snackbar} setData={ShowSnackbar} />
+        <div class="container-fluid">
+          <div class="col mt-2 d-flex justify-content-between">
+            <h1 class="d-inline-block">Customers</h1>
+          </div>
+        </div>
+        <Box sx={{ mx: 2 }} className="border">
+          <Table>
+            <TableHead>
+              <TableRow>
+                {/* <TableCell className="w-12"><b>S.No</b></TableCell> */}
+                <TableCell className="w-25">
+                  <b>Name</b>
+                </TableCell>
+                <TableCell>
+                  <b>Email</b>
+                </TableCell>
+                <TableCell>
+                  <b>Phone No.</b>
+                </TableCell>
+                <TableCell>
+                  <b>Active</b>
+                </TableCell>
+                <TableCell>
+                  <b>Actions</b>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {collection.map((res, index) => {
+                return (
+                  <TableRow key={index}>
+                    {/* <TableCell>{index + 1}</TableCell> */}
+                    <TableCell className="text_cap">{res.fullName}</TableCell>
+                    <TableCell>{res.email}</TableCell>
+                    <TableCell>{res.mobile_number}</TableCell>
+                    <TableCell>
+                      <Switch
+                        defaultChecked={res.user_login}
+                        onChange={() => active(res._id)}
+                      />
+                    </TableCell>
+                    <TableCell>
+                      {/* <RemoveRedEye  /> */}
+                      <Delete
+                        className="pointer"
+                        onClick={(e) => deleteusers(res._id)}
+                      />
+                      &nbsp;&nbsp;
+                      {/* <Edit className="pointer" /> */}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+          <Box sx={{ m: 1 }} className="d-flex justify-content-end">
+            <select
+              className="me-2"
+              onChange={(e) => setCountPerPage(e.target.value * 1)}
+            >
+              <option>5</option>
+              <option>10</option>
+              <option>15</option>
+            </select>
+            <Pagination
+              pageSize={countPerPage}
+              onChange={updatePage}
+              current={currentPage}
+              total={allUsers.length}
+              style={{ color: "green" }}
+            />
+          </Box>
+        </Box>
+      </>
+    );
 }
