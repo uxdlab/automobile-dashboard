5e8730f2813022568ded2e4d6eb955b8d31fba1d
Yeshraj@12 <yeshraj@uxdlabtech.com>
2023-07-13 10:03:26 +0530

    payment listing

--- src/Components/Payment/PaymentListing.jsx
+++ src/Components/Payment/PaymentListing.jsx
@@ -67,102 +67,6 @@ export default function PaymentListing() {
           {/* <input type="search" placeholder='Search here' class="search_box mt-1 border"></input> */}
         </div>
       </div>
-      <>
-        {/* <div class="container-fluid">
-        <div class="row">
-            <div class="col">
-            <div class="table-responsive">
-            <table class="table my-3 border">
-                <thead class="thead-light">
-                <tr>
-                    <th scope="col">OrderID</th>
-                    <th scope="col">Date</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Subscription Type</th>
-                    <th scope="col">Subscription</th>
-                    <th scope="col">Amount</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">View</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>Order_Ldfdyd5dfg8sfg5</td>
-                    <td>Feb 15,2023</td>
-                    <td>Akash</td>
-                    <td>Subject</td>
-                    <td>Physics</td>
-                    <td>1000</td>
-                    <td>Success</td>
-                    <td>...</td>
-                </tr>
-                <tr>
-                    <td>Order_Ldfdyd5dfg8sfg5</td>
-                    <td>Feb 15,2023</td>
-                    <td>Rajeev</td>
-                    <td>Subject</td>
-                    <td>Physics</td>
-                    <td>1000</td>
-                    <td>Success</td>
-                    <td>...</td>
-                </tr>
-                <tr>
-                    <td>Order_Fgf565d5dfg8sfg5</td>
-                    <td>Feb 15,2023</td>
-                    <td>Rajeev</td>
-                    <td>Subject</td>
-                    <td>Alpha Testing</td>
-                    <td>150</td>
-                    <td>Test</td>
-                    <td>...</td>
-                </tr>
-                <tr>
-                    <td>Order_Tfg45f45dfg8sfg5</td>
-                    <td>Feb 15,2023</td>
-                    <td>Ashok</td>
-                    <td>Subject</td>
-                    <td>Alpha Testing</td>
-                    <td>100</td>
-                    <td>Test</td>
-                    <td>...</td>
-                </tr>
-                <tr>
-                    <td>Order_Fgf565d5dfg8sfg5</td>
-                    <td>mar 20,2023</td>
-                    <td>Ankush</td>
-                    <td>Subject</td>
-                    <td>Alpha Testing</td>
-                    <td>100</td>
-                    <td>Test</td>
-                    <td>...</td>
-                </tr>
-                <tr>
-                    <td>Order_Fgf565d5dfg8sfg5</td>
-                    <td>Feb 15,2023</td>
-                    <td>Rajeev</td>
-                    <td>Subject</td>
-                    <td>Alpha Testing</td>
-                    <td>15</td>
-                    <td>Alpha_Testing</td>
-                    <td>...</td>
-                </tr>
-                <tr>
-                    <td>Order_R54f5gf5dfg8sfg5</td>
-                    <td>Feb 15,2023</td>
-                    <td>Rajeev</td>
-                    <td>Subject</td>
-                    <td>Alpha Testing</td>
-                    <td>250</td>
-                    <td>Alpha_Testing</td>
-                    <td>...</td>
-                </tr>
-                </tbody>
-            </table>
-            </div>
-            </div>
-        </div>
-    </div> */}
-      </>
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
@@ -170,16 +74,7 @@ export default function PaymentListing() {
               <TableCell className="text-center">
                 <b>OrderID</b>
               </TableCell>
-              <TableCell className="text-center">
-                <b>Name</b>
-              </TableCell>
 
-              <TableCell className="text-center">
-                <b>Email</b>
-              </TableCell>
-              <TableCell className="text-center">
-                <b>Phone Number</b>
-              </TableCell>
               <TableCell className="text-center">
                 <b>Amount</b>
               </TableCell>
@@ -198,16 +93,7 @@ export default function PaymentListing() {
                   <TableCell className="text_cap text-center">
                     {res.orderId}
                   </TableCell>
-                  <TableCell className="text_cap text-center">
-                    {res.buyerName}
-                  </TableCell>
 
-                  <TableCell className="text_cap text-center">
-                    {res.buyerEmail}
-                  </TableCell>
-                  <TableCell className="text_cap text-center">
-                    {res.buyerPhone}
-                  </TableCell>
                   <TableCell className="text_cap text-center">
                     {res.amount}
                   </TableCell>
